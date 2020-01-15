0a7100efac1b2a569380c3b7911a3bf16657bf5a
Pierre <pperruch@ens-lyon.fr>
2020-01-15 03:06:30 +0000

    Complete redo, including change of language.

--- perso.js
+++ perso.js
@@ -12,25 +12,14 @@ function changerInfo(texte){
   }
 
 $(document).ready(function(){
-  var besoinDeCliquer = true;
-  
-  $(window).on("load hashchange",function(){
-    if(window.location.hash) {
-      var cible = window.location.hash.substring(1);
-    } else{
-      var cible = $("#barre li:first-child a").attr("href").substr(1);
-    }
-    if(besoinDeCliquer){$("a[href='#"+cible+"']").click();}
-    besoinDeCliquer = true;
-    });
-  
   $(".adresse").mouseenter(function(){
     changerInfo("Pierre Perruchaud<br>\
-                 255 Hurley Building<br>\
+                 Office: Hayes-Healy 202<br>\
+                 Mail: 255 Hurley Building<br>\
                  Notre Dame, IN 46556-4618");
     });
   $(".adresse").click(function(){
-    alert("Pierre Perruchaud\n255 Hurley Building\nNotre Dame, IN 46556-4618");
+    alert("Pierre Perruchaud\nOffice: Hayes-Healy 202\nMail: 255 Hurley Building\nNotre Dame, IN 46556-4618");
     });
   
   $(".mail").mouseenter(function(){
@@ -51,33 +40,5 @@ $(document).ready(function(){
     changerInfo("");
     });
   
-  $(".onglet").click(function(){
-    var cible = $(this).attr("href").substr(1);
-    $("#page").load(cible + ".html",function(){
-      $(".mini-onglet").click(function(e){
-        e.preventDefault();
-        $(".mini-contenu.active").slideUp(150);
-        $(".mini-contenu.active").removeClass("active");
-        $(".mini-onglet.active").removeClass("active");
-        var cible = $(this).attr("href");
-        $(cible).slideDown(150);
-        $(cible).addClass("active")
-        $(this).addClass("active")
-        });
-     $(".mini-onglet:first").click();
-     $(".deroulable .accroche").click(function(){
-        $(this).children("i").toggleClass("glyphicon-menu-right");
-        $(this).children("i").toggleClass("glyphicon-menu-down");
-        $(this).next().slideToggle(150);
-        });
-      $("a.email").attr("href",function(){
-        return "mailto:" + emailfromdata($(this).attr("data-nom"),
-                                         $(this).attr("data-adresse"),
-                                         $(this).attr("data-domaine"));
-        });
-      MathJax.Hub.Queue(["Typeset",MathJax.Hub,"contenu"]);
-      });
-    besoinDeCliquer = false;
-    document.location.hash = cible;
-   });
+  MathJax.Hub.Queue(["Typeset",MathJax.Hub,"contenu"]);
  });
